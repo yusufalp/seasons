1612ddad0f1d63f0e664d8c38ef8dd549349d1d5
Yusuf Alp <alpyusuf007@gmail.com>
2020-12-15 16:01:42 -0800

    added state management and dynamic season change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import SeasonDisplay from './SeasonDisplay';
+import Loading from './Loading';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    lat: null,
+    errorMessage: ''
+  }
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  contentDecision() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <p>{this.state.errorMessage}</p>;
+    } else if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    } else {
+      return <Loading message='Please allow location sharing...' />;
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Current Season</h1>
+        {this.contentDecision()}
+      </div>
+    );
+  }
 }
 
 export default App;
